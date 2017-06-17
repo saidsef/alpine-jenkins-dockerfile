1c0860ad7dc352e7e3ce3af0599b24b5d5c2cee0
Said Sef <saidsef@gmail.com>
2017-06-17 17:55:34 +0100

    updated init groovy script to skip installation wizard

--- groovy/init.groovy
+++ groovy/init.groovy
@@ -1,4 +1,6 @@
 #!groovy
+// https://mrooding.me/dockerized-jenkins-2-on-google-cloud-platform-34747725e786
+// https://go.cloudbees.com/docs/cloudbees-documentation/admin-instance/setup/
 
 import jenkins.model.*
 import hudson.security.*
@@ -12,5 +14,6 @@ hudsonRealm.createAccount('admin','admin')
 instance.setSecurityRealm(hudsonRealm)
 
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+strategy.setAllowAnonymousRead(false)
 instance.setAuthorizationStrategy(strategy)
 instance.save()

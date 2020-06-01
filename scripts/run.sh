d1ee2e3c6d054fb96da91e6fe08aa0a627f1d8d4
Said Sef <saidsef@gmail.com>
2020-06-01 22:28:57 +0100

    updated run script to create resource in local user space

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 
-BUILD_ID=$1
+TAG=$1
+BASE=$HOME
 
 function usage {
   echo """
@@ -12,23 +13,23 @@ function usage {
 
 function run {
   echo "Create Jenkins CI/CD spaces"
-  mkdir -p /mnt/jenkins/{workspace,jobs,cache,logs,secrets}
+  mkdir -p $BASE/jenkins/{workspace,jobs,cache,logs,secrets}
 
   echo "Update permissions"
-  chmod 777 -R /mnt/jenkins/{workspace,jobs,cache,logs,secrets}
+  chmod 777 -R $BASE/jenkins/{workspace,jobs,cache,logs,secrets}
 
   echo "Run Jenkins CI/CD"
   docker run -d -p 9099:8080 \
-  -v /mnt/jenkins/workspace:/var/jenkins_home/workspace \
-  -v /mnt/jenkins/jobs:/var/jenkins_home/jobs \
-  -v /mnt/jenkins/cache:/var/jenkins_home/cache \
-  -v /mnt/jenkins/logs:/var/jenkins_home/jenkins-jobs \
-  -v /mnt/jenkins/secrets:/var/jenkins_home/secrets \
-  saidsef/alpine-jenkins-dockerfile:${BUILD_ID}
+  -v $BASE/jenkins/workspace:/var/jenkins_home/workspace \
+  -v $BASE/jenkins/jobs:/var/jenkins_home/jobs \
+  -v $BASE/jenkins/cache:/var/jenkins_home/cache \
+  -v $BASE/jenkins/logs:/var/jenkins_home/jenkins-jobs \
+  -v $BASE/jenkins/secrets:/var/jenkins_home/secrets \
+  saidsef/alpine-jenkins-dockerfile:${TAG}
 }
 
 function main {
-  if [ -z "${BUILD_ID}" ]; then
+  if [ -z "${TAG}" ]; then
     usage
   else
     run

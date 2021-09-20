0d1d0e7c82063794cf91912db352945902a5706d
Said Sef <saidsef@gmail.com>
2021-09-20 14:29:01 +0100

    updated image name and tag vars

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-set -ex
+set -e
 
-TAG=$1
+TAG=${$1:-latest}
 
 function deploy {
   echo "Deploy image to docker hub"
-  docker push docker.io/saidsef/alpine-jenkins-dockerfile:$TAG
+  docker push docker.io/saidsef/${PWD##*/}:$TAG
 }
